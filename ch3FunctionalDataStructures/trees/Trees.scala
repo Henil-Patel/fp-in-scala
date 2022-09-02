49feac3eda624ccd64aeea76c09b10e3e21b885e
Henil-Patel <55055310+Henil-Patel@users.noreply.github.com>
2022-09-02 17:46:26 -0400

    Added problem statements

--- ch3FunctionalDataStructures/trees/Trees.scala
+++ ch3FunctionalDataStructures/trees/Trees.scala
@@ -3,12 +3,23 @@ case class Leaf[A](value: A) extends Tree[A]
 case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
 object Tree {
+
+  /*
+  * Exercise 25: Write a function size that counts the number of nodes (leaves and branches)
+  * in a tree.
+  * */
   def size[A](t: Tree[A]): Int = {
     t match {
       case Leaf(_) => 1
       case Branch(l ,r) => 1 + size(l) + size(r)
     }
   }
+
+  /*
+  * Exercise 26: Write a function maximum that returns the maximum element in a Tree[Int].
+  * (Note: In Scala, you can use x.max(y) or x max y to compute the maximum of two integers
+  * x and y.)
+  * */
 }
 
 @main
