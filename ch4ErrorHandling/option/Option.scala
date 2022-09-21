c6ff234f9c71602c47867814d42e7c35f0a369ed
Henil-Patel <55055310+Henil-Patel@users.noreply.github.com>
2022-09-21 15:13:39 -0400

    Error Handling chap started

---
+++ ch4ErrorHandling/option/Option.scala
@@ -0,0 +1,19 @@
+
+sealed trait Option[+A]
+case class Some[+A](get: A) extends Option[A]
+case object None extends Option[Nothing]
+
+def mean(xs: Seq[Double]): Option[Double] = {
+  if (xs.isEmpty) None
+  else Some(xs.sum / xs.length)
+}
+
+@main
+def mainF(): Unit = {
+  val sq: Seq[Double] = Seq(1.0, 2.0, 3.0)
+  val mt: Seq[Double] = Seq()
+  val retSq = mean(sq)
+  val retMt = mean(mt)
+  println("Mean of sq: " + retSq)
+  println("Mean of mt: " + retMt)
+}
